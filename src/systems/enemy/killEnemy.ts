6886f03028f80f96ec5cb15c87b73c65573e628b
Arek Rado <arekrrr@gmail.com>
2024-02-10 15:22:05 +0700

    ,

--- src/systems/enemy/killEnemy.ts
+++ src/systems/enemy/killEnemy.ts
@@ -1,9 +1,12 @@
 import { Entity } from '@arekrado/canvas-engine'
 import { scene } from '../../main'
+import { getStore } from '../../utils/store'
 
 export const killEnemy = (entity: Entity) => {
   const e = scene.getMeshByName(entity)
   console.log(e)
+
+  getStore().removeEntity(entity)
 
   // const ragdoll = new Ragdoll(skeleton, newMeshes[0], config);
 }
